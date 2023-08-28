eb09dce0a55ef4efe1f8cc7823e69c31d5bbb0a2
dahye <bbngsk@gmail.com>
2023-08-28 09:10:46 +0900

    Initial Commit

---
+++ PhotoLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoLog
+//
+//  Created by 박다혜 on 2023/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
