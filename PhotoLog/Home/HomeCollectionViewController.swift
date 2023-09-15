1b9f54f3ad5ae8512c6d08979d99990d224d4b35
dahye <bbngsk@gmail.com>
2023-09-15 23:06:36 +0900

    이미지가 안나온다...

---
+++ PhotoLog/Home/HomeCollectionViewController.swift
@@ -0,0 +1,98 @@
+//
+//  HomeCollectionViewController.swift
+//  PhotoLog
+//
+//  Created by 박다혜 on 2023/09/15.
+//
+
+import UIKit
+import SnapKit
+import Kingfisher
+
+class HomeCollectionViewController: BaseViewController {
+
+    //var list: Photo = Photo(total: 0, total_pages: 0, results: [])
+    var list: [PhotoResults] = []
+    let mainView = HomeView()
+    let pic = UIImageView()
+
+    var cellRegistration: UICollectionView.CellRegistration<UICollectionViewListCell, PhotoResults>!
+
+    override func loadView() {
+        mainView.delegate = self
+        self.view = mainView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mainView.collectionView.delegate = self
+        mainView.collectionView.dataSource = self
+
+        cellRegistration = UICollectionView.CellRegistration(handler: { cell, indexPath, itemIdentifier in
+            var content = UIListContentConfiguration.valueCell()
+            guard let url = URL(string: itemIdentifier.urls.thumb) else { return }
+
+            self.downloadImage(with: url) { image in
+                content.image = image
+            }
+
+            cell.contentConfiguration = content
+        })
+
+        APIService.shared.request(query: "sky") { [weak self] photo in
+
+            guard let photo = photo else {
+                print("alert error")
+                return
+            }
+
+            self?.list = photo.results
+            self?.mainView.collectionView.reloadData()
+        }
+
+    }
+
+    override func configureView() {
+        super.configureView()
+
+    }
+
+    func downloadImage(with url: URL, completionHandler: @escaping (UIImage) -> Void) {
+        let resource = KF.ImageResource(downloadURL: url)
+
+        KingfisherManager.shared.retrieveImage(with: resource){ result in
+
+            switch result{
+            case .success(let value):
+                completionHandler(value.image)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+}
+
+extension HomeCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        list.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let data = list[indexPath.item]
+        let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: data)
+        return cell
+    }
+
+}
+
+extension HomeCollectionViewController: HomeViewProtocol {
+
+    func didSelectItemAt(indexPath: IndexPath) {
+        print(indexPath)
+        navigationController?.popViewController(animated: true)
+    }
+
+}
